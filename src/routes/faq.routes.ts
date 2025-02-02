db837ad21f51bed6bfc5b24d2c4bd4803000f3f9
Shvam Singh <singhshivam583@yahoo.in>
2025-02-02 12:27:57 +0530

    fix: modify faqRoute

--- src/routes/faq.routes.ts
+++ src/routes/faq.routes.ts
@@ -3,8 +3,10 @@ import { getFaqs, getFaqById, createFaq } from "../controllers/faq.controller";
 
 const faqRouter = Router();
 
-faqRouter.route('/').get(getFaqs);
+faqRouter.route('/')
+.get(getFaqs)
+.post(createFaq);
+
 faqRouter.route('/:id').get(getFaqById);
-faqRouter.route('/').post(createFaq);
 
 export default faqRouter;
